15dd9ce844600de7fcd9c6a513df47e7d4f8f229
Paul Aji <paulajiparayil123@gmail.com>
2023-09-14 13:53:05 +0530

    combined all three apis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import facebook
+import requests
 
 app = Flask(__name__)
 
@@ -59,6 +60,25 @@ def fetch_instagram_accounts():
         return render_template('instagram_info.html', **context)
     except facebook.GraphAPIError as e:
         error_message = str(e)
+        return render_template('error.html', error=error_message)
+
+@app.route('/linkedin')
+def fetch_linkedin_accounts():
+    api_key = ''
+    headers = {'Authorization': 'Bearer ' + api_key}
+    api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+    linkedin_profile_url = 'https://www.linkedin.com/in/paulaji'  # Replace with the desired LinkedIn profile URL
+
+    response = requests.get(api_endpoint,
+                            params={'url': linkedin_profile_url},
+                            headers=headers)
+
+    if response.status_code == 200:
+        linkedin_data = response.json()
+
+        return render_template('linkedin_info.html', linkedin_data=linkedin_data)
+    else:
+        error_message = f"Request failed with status code: {response.status_code}"
         return render_template('error.html', error=error_message)
 
 if __name__ == "__main__":
